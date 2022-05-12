609f841448e0348a74124f231f28c2c2ae1e6947
Baker El Muhur <b.elmuhur@gmail.com>
2022-05-12 15:09:26 +0200

    Codelab 07 make it work with production (hopefully)

--- src/app/helpers/helper-functions.ts
+++ src/app/helpers/helper-functions.ts
@@ -1,11 +1,12 @@
 import {Pet} from "../model/Pet";
 import {Subscription} from "rxjs";
+import {environment} from "../../environments/environment";
 
 const localImagesUrl = 'http://localhost:4200/assets/';
 const remoteImagesUrl = 'https://pettinder.herokuapp.com/';
 
 export function imageDomain(pet: Pet): string {
-  if (pet.id > 7 || pet.id === undefined) {
+  if ((pet.id > 7 || pet.id === undefined) && environment.production) {
     return localImagesUrl
   }
   return remoteImagesUrl
